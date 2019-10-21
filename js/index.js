66a2e0e26e2a8c2cf4fc2abaf6a38970bced3268
Eliseu Baldo <eliseu.baldo@gmail.com>
2019-10-21 09:04:24 -0400

    trying to expose namespace

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ import Bus from './bus.js';
 // add new "service" to load promise some passengers for the bus
 // random generate map with places to click
 
-window.moveBus = function moveBus() {
+  function moveBus() {
   let busname = document.getElementById('busname').value;
   let x = Number(document.getElementById('xcoord').value);
   let y = Number(document.getElementById('ycoord').value);
@@ -16,12 +16,24 @@ window.moveBus = function moveBus() {
 
 }
 
+window.LATM = {
+  moveBus: moveBus(),
+  initiate: initiate(amount)
+}
 
+
+function initiate(amount) {
   let busArray = [];
+  const colorNames = ['Silver', 'Red', 'Wolf', 'Grey', 'Master', 'Amtrak', 'Beagle'];
+  const funnyNames = ['Lessard', 'Canada', 'Knight', 'XPO', 'Swift', 'American', 'Lake'];
+  rnd = Math.floor(Math.random() * colorNames.length());
+  console.log(rnd);
+
+
   busArray.push(new Bus("eli", "red", 50, 6, 10));
   busArray.push(new Bus("MTL", "green",100,  250, 50));
   console.log(busArray);
-
+}
 
 
 
